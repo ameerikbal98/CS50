373e881192e2a90616abe7d5efa88bd168dafd5f
Ameer Ikbal <ameeriqbal2083@gmail.com>
2025-03-30 22:07:58 +0100

    update mario.c

--- week 2/mario.c
+++ week 2/mario.c
@@ -1,6 +1,17 @@
 #include "cs50/cs50.h"
 #include <stdio.h>
 
+
+/*
+In a file called mario.c in a folder called mario-more, implement a program in C that recreates that pyramid, using hashes (#) for bricks, as in the below:
+
+   #  #
+  ##  ##
+ ###  ###
+####  ####
+
+And let’s allow the user to decide just how tall the pyramids should be by first prompting them for a positive int between, say, 1 and 8, inclusive.
+*/
 int main()
 {
     int n;
